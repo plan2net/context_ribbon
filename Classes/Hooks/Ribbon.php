2bbe8ad043b388a98adccf4f68b689865371262f
Ioulia Kondratovitch <ik@plan2.net>
2020-11-09 19:42:22 +0100

    [FEATURE] Move the whole logic to a single class

--- Classes/Hooks/Ribbon.php
+++ Classes/Hooks/Ribbon.php
@@ -8,8 +8,11 @@ use Psr\Log\LoggerAwareTrait;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Core\ApplicationContext;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use WapplerSystems\ContextRibbon\Helper\ContextHelper;
+use TYPO3\CMS\Core\Utility\PathUtility;
 
 /**
  * Class Ribbon
@@ -21,6 +24,16 @@ use WapplerSystems\ContextRibbon\Helper\ContextHelper;
 class Ribbon implements LoggerAwareInterface
 {
     use LoggerAwareTrait;
+
+    /**
+     * @var PageRenderer
+     */
+    protected $pageRenderer;
+
+    public function __construct()
+    {
+        $this->pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+    }
 
     public function setRibbonForBackend(): void
     {
@@ -43,10 +56,54 @@ class Ribbon implements LoggerAwareInterface
             $this->logger->error($extensionConfigurationPathDoesNotExistException->getMessage());
         }
         if ($configurationEntry) {
-            $contextHelper = GeneralUtility::makeInstance(ContextHelper::class);
-            $contextHelper->addMetaTag();
-            $contextHelper->addCssJsFiles();
+            $this->addMetaTag();
+            $this->addCssJsFiles();
         }
+    }
+
+    /**
+     * Provide HTML with an information about TYPO3_CONTEXT:
+     * add a meta tag to the header data.
+     */
+    protected function addMetaTag(): void
+    {
+        $strContext = '';
+        /** @var ApplicationContext $context */
+        $context = Environment::getContext();
+
+        if (isset($context) && ($context->__toString() === 'Production/Staging' || $context->__toString() === 'Development/Staging')) {
+            $strContext = 'staging';
+        } elseif ($context->isDevelopment()) {
+            $strContext = 'development';
+        } elseif ($context->isTesting()) {
+            $strContext = 'testing';
+        } elseif (TYPO3_MODE === 'BE' && $context->isProduction()) {
+            $strContext = 'production';
+        }
+
+        $this->pageRenderer->addHeaderData('<meta name="context" content="' . $strContext . '" />');
+    }
+
+    /**
+     * Add CSS and JS files
+     */
+    protected function addCssJsFiles(): void
+    {
+        $this->pageRenderer->addCssFile(
+            PathUtility::getAbsoluteWebPath(
+                GeneralUtility::getFileAbsFileName(
+                    'EXT:context_ribbon/Resources/Public/CSS/ribbon.css'
+                )
+            )
+        );
+
+        $this->pageRenderer->addJsFile(
+            PathUtility::getAbsoluteWebPath(
+                GeneralUtility::getFileAbsFileName(
+                    'EXT:context_ribbon/Resources/Public/JavaScript/ribbon.js'
+                )
+            )
+        );
     }
 
 }
