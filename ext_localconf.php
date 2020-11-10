6e68c6cce6dd76a2bf602e0ef55472ee98742bcf
Wolfgang Klinger <wk@plan2.net>
2020-11-10 07:26:56 +0100

    [BUGFIX] Use Ribbon::class for keys and unify ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,8 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') or die('Access denied.');
+
 (static function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/backend.php']['renderPreProcess']['WaplerSystems\\ContextRibbon\\Hooks\\Ribbon'] = \WapplerSystems\ContextRibbon\Hooks\Ribbon::class . '->setRibbonForBackend';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['configArrayPostProc']['WaplerSystems\\ContextRibbon\\Hooks\\Ribbon'] = \WapplerSystems\ContextRibbon\Hooks\Ribbon::class . '->setRibbonForFrontend';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/backend.php']['renderPreProcess'][\WapplerSystems\ContextRibbon\Hooks\Ribbon::class] = \WapplerSystems\ContextRibbon\Hooks\Ribbon::class . '->setRibbonForBackend';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['configArrayPostProc'][\WapplerSystems\ContextRibbon\Hooks\Ribbon::class] = \WapplerSystems\ContextRibbon\Hooks\Ribbon::class . '->setRibbonForFrontend';
 })();
