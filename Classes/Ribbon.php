7bf62a3e6e6f0062a02589e33fedc94fb2ce0e38
Richard Krikler <rk@plan2.net>
2025-03-06 08:51:35 +0100

    [BUGFIX] Check if $applicationType is null before using it to check if request originates from backend

--- Classes/Ribbon.php
+++ Classes/Ribbon.php
@@ -92,7 +92,7 @@ class Ribbon
             $contextName = 'development';
         } elseif ($context->isTesting()) {
             $contextName = 'testing';
-        } elseif ($applicationType->isBackend() && $context->isProduction()) {
+        } elseif ((null !== $applicationType && $applicationType->isBackend()) && $context->isProduction()) {
             $contextName = 'production';
         }
 
